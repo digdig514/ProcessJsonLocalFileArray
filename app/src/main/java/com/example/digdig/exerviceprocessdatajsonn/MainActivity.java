89a3ee4a4504b67f6920faad5843e384f3d6ad8e
digdig514 <diegosouzasampaio@gmail.com>
2017-06-20 12:46:22 -0400

    first sconed

--- app/src/main/java/com/example/digdig/exerviceprocessdatajsonn/MainActivity.java
+++ app/src/main/java/com/example/digdig/exerviceprocessdatajsonn/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.digdig.exerviceprocessdatajsonn;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.digdig.exerviceprocessdatajsonn.model.JSONManagement;
+import com.example.digdig.exerviceprocessdatajsonn.model.JSONManagementArray;
+import com.example.digdig.exerviceprocessdatajsonn.model.SuperHero;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button btnProcessJSONFile;
+    Button btnProcessJSONFile,BtnProcessJSONFileArray;
+    ListView listViewMembers;
+    ArrayAdapter<SuperHero> arrayAdapter;
+    ArrayList<SuperHero> listOfMembers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +27,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initialize() {
 
-
+        listViewMembers = (ListView) findViewById(R.id.ListView);
         btnProcessJSONFile = (Button)findViewById(R.id.btnProcessJSONFile);
-
-
-
-        btnProcessJSONFile.setOnClickListener(this);
+       btnProcessJSONFile.setOnClickListener(this);
+        BtnProcessJSONFileArray = (Button)findViewById(R.id.btnProcessJSONFileArray);
+        BtnProcessJSONFileArray.setOnClickListener(this);
 
     }
 
@@ -31,9 +39,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
         case R.id.btnProcessJSONFile :
-        JSONManagement.processJSONFile(this,"JsonFileForExercise.json");
+            JSONManagement.processJSONFile(this,"JsonFileForExercise.json");
         break;
-        
+            case R.id.btnProcessJSONFileArray :
+        listOfMembers = JSONManagementArray.processJSONFile(this,"JsonFileForExercise.json");
+                arrayAdapter = new ArrayAdapter<SuperHero>(this,android.R.layout.simple_list_item_1,listOfMembers);
+                listViewMembers.setAdapter(arrayAdapter);
+                //listViewMembers.setOnItemClickListener(this);
+                break;
     }
     }
 }
